39e9090c198d34585685ed08cdcec2ac4c0f8dcb
seantamturk <128218653+seantamturk@users.noreply.github.com>
2023-04-03 20:10:54 -0700

    added psuedo code and questions

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+// click start to start timer
+    //count down from 60
+//question appears on screen
+    //4 options below
+    //on wrong answer, reduce time by 10 seconds
+    //on correct, go to next question and add +1 to the score
+    //remove finished question from array of questions
+//when time = 0, end game
+//prompt for initials
+//score and initials entered into scoreboard
+
+// Questions array
+var questions = ["Commonly used data types do not include:", 
+    "The condition in an if / else statement is enclosed with ____", 
+    "Arrays in Javascript can be used to store ____", 
+    "String values must be enclosed within ____ when being assigned to variables.",
+    "A very useful tool used during development and debugging for printing content to the debugger is:"];
+
+// Answers array
+var answers = [""
+
+]
